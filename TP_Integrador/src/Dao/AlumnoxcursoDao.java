35c552fd6f807b9c6676d4eea9a15bfd2e067dca
Hest10 <hermest10@aol.com>
2021-12-12 00:47:27 -0300

    Entidad y daoimpl
    Entidad alumnoxcurso + daoimpl Alumnoxcurso

---
+++ TP_Integrador/src/Dao/AlumnoxcursoDao.java
@@ -0,0 +1,12 @@
+package Dao;
+
+import java.util.ArrayList;
+
+import Entidad.Alumnoxcurso;
+
+public interface AlumnoDaoxcurso {
+	public int agregarAlumnoxcurso(Alumnoxcurso alumnoxcurso);
+	public ArrayList<Alumnoxcurso> ListarAlumnosxcurso();
+	public int eliminarAlumnoxcurso(int id);
+	public Alumno obtenerAlumno(int id);
+}
